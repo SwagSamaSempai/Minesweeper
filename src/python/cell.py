c4d3631118901305924a1c48985e25a47bd4e4ba
Philémon van Helden <philemon.vanhelden@gmx.com>
2021-08-22 15:56:25 +0200

    [IMP] Added bomb generation

--- src/python/cell.py
+++ src/python/cell.py
@@ -8,5 +8,12 @@ class Cell(Button):
     def __init__(self, x, y, **kwargs):
         self.x = x
         self.y = y
+        self.value = 0
 
         super(Cell, self).__init__(**kwargs)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
--- src/python/grid.py
+++ src/python/grid.py
@@ -1,3 +1,5 @@
+from random import sample
+
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 
@@ -10,9 +12,17 @@ class Grid(GridLayout):
     def __init__(self, width, height, bombs_count, **kwargs):
         self.cols = width
         self.rows = height
+        self.cells = []
 
         super(Grid, self).__init__(**kwargs)
 
-        self.cells = [Cell(x, y) for x in range(width) for y in range(height)]
-        for cell in self.cells:
-            self.add_widget(cell)
+        bombs = sample(range(width * height), bombs_count)
+        for y in range(height):
+            row = []
+            for x in range(width):
+                cell = Cell(x, y)
+                if y * width + x in bombs:
+                    cell.value = 'B'
+                self.add_widget(cell)
+                row.append(cell)
+            self.cells.append(row)
